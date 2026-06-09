a9f0f2b1b0a735cbe9bcb14ae84e902dfd7d149f
meo <chenevard.romeo@gmail.com>
2026-06-09 22:57:14 +0200

    build: move test deps into version catalog for consistency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.datastore.preferences)
 
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
+    testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
 }
